2b641c498d5058fc6c2152c4fc8a81f2106facb1
Adam <Adam@DESKTOP-QSHAJVS.Home>
2020-11-25 21:52:40 +0500

    just checking a thing

--- assignmen2/src/assignmen2/bodyofmain.java
+++ assignmen2/src/assignmen2/bodyofmain.java
@@ -5,6 +5,7 @@ public class bodyofmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("hello this is start of game \n Black player is x and whitel player o");
+		int x=0;
 
 	}
 
